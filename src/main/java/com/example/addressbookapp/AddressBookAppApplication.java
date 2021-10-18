ce060272f3667b3d47c2b22b0ed915093e927d81
SW-GAGAN <srgaganreddy0@gmail.com>
2021-10-18 10:39:03 +0530

    Adding : REST Request from AddressBook UI

--- src/main/java/com/example/addressbookapp/AddressBookAppApplication.java
+++ src/main/java/com/example/addressbookapp/AddressBookAppApplication.java
@@ -3,11 +3,12 @@ package com.example.addressbookapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class AddressBookAppApplication {
 
     public static void main(String[] args) {
-
+        System.out.println("Welcome to AddressBook App!!");
         SpringApplication.run(AddressBookAppApplication.class, args);
     }
 
